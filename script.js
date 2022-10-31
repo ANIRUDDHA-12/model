642c593e6ed651951cd54526ab5559924233fe5e
Aniruddha-12 <payas.sanjay@gmail.com>
2022-10-31 22:21:56 +0530

    completed

---
+++ script.js
@@ -0,0 +1,12 @@
+let picture=document.getElementById("pic")
+let box=document.getElementById("box")
+
+function display(){
+picture.style.display="block";
+box.style.display="block";
+}
+function closed(){
+    picture.style.display="none";
+    box.style.display="none";
+}
+
